415b4da36af30587e23026afe4fab23b0b3aa992
Carl <76235678+notpikmin@users.noreply.github.com>
2024-08-05 13:35:33 -0700

    Update webhook.go

--- webhook.go
+++ webhook.go
@@ -56,8 +56,8 @@ func CreateEmbed(user User, score int) *discordwebhook.Embed {
 }
 
 func SendEmbed(user User, score int) {
-	var url = "https://discord.com/api/webhooks/1005687599286976522/6Ferdnfxy4sBFYhMWxqTQDWjJDxaueq2QuyiCQVA0G1NnnZKoCf_-Mk6eLcW8qCha8Bo"
-	//var url = "https://discord.com/api/webhooks/1246980704370163724/lJAKIEzicMkPhynEx7SAoEFxKc5MDNcSUTkGI-rFPX1B041_oqLvdUTAF6888Yu9x3FI"
+	var url = ""
+	//var url = ""
 
 	embed := CreateEmbed(user, score)
 	message := discordwebhook.Message{
@@ -73,7 +73,7 @@ func SendEmbed(user User, score int) {
 
 }
 func Send(user User, score int) {
-	var url = "https://discord.com/api/webhooks/1248333994421125223/hxmZPzrTIgu6mGIZ1FXcaeGjx61qGJ5ksJL53HnuaOM0k5038vllLIwh6_NOw8Id0dLD"
+	var url = ""
 
 	embed := CreateEmbed(user, score)
 	message := discordwebhook.Message{
